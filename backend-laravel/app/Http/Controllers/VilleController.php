24a32b24fb1da4894685b87f64faaf6e2f7fb0f6
Thombrix <bruno.thomas-caruso@outlook.com>
2023-10-29 20:56:25 +0100

    Added Swagger

--- backend-laravel/app/Http/Controllers/VilleController.php
+++ backend-laravel/app/Http/Controllers/VilleController.php
@@ -8,8 +8,37 @@ use App\Models\Cycle;
 
 class VilleController extends Controller
 {
+    /**
+     * Get the current Ville.
+     *
+     * @OA\Get(
+     *     path="/api/ville/today",
+     *     summary="Get the current city of the day",
+     *     description="Retrieve information about the current city.",
+     *     operationId="getToday",
+     *     tags={"Ville"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *         @OA\JsonContent(ref="#/components/schemas/Ville")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Ville not found"
+     *     )
+     * )
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function today()
     {
-        return Cycle::getCurrentVille();
+        $ville = Cycle::getCurrentVille();
+
+        if(!$ville)
+        {
+            return response()->json(['error' => 'Ville not found'], 404);
+        }
+
+        return $ville;
     }
 }
--- backend-laravel/app/Models/Ville.php
+++ backend-laravel/app/Models/Ville.php
@@ -5,6 +5,36 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Ville",
+ *     type="object",
+ *     @OA\Property(
+ *         property="name",
+ *         type="string"
+ *     ),
+ *     @OA\Property(
+ *         property="img",
+ *         type="string"
+ *     ),
+ *     @OA\Property(
+ *         property="ecusson",
+ *         type="string"
+ *     ),
+ *     @OA\Property(
+ *         property="population",
+ *         type="integer"
+ *     ),
+ *     @OA\Property(
+ *         property="coord",
+ *         type="string"
+ *     ),
+ *     @OA\Property(
+ *         property="canton_id",
+ *         type="int"
+ *     )
+ * )
+ */
 class Ville extends Model
 {
     use HasFactory;
