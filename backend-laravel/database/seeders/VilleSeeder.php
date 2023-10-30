8cee2c5a617fd8638fda76bf561280412cbaa0d7
loic.santschi <loic.santschi@he-arc.ch>
2023-10-30 17:47:49 +0100

    added some urls in the seeder for the city image

--- backend-laravel/database/seeders/VilleSeeder.php
+++ backend-laravel/database/seeders/VilleSeeder.php
@@ -13,10 +13,10 @@ class VilleSeeder extends Seeder
     public function run(): void
     {
         $cities = [
-            ['name' => 'Lausanne', 'img' => 'lausanne.png', 'ecusson' => 'lausanne-ecu.png', 'population' => 140000, 'coord' => '46.519961, 6.633597', 'canton_id' => 23],
-            ['name' => 'Zurich', 'img' => 'zurich.png', 'ecusson' => 'zurich-ecu.png', 'population' => 400000, 'coord' => '47.376886, 8.541694', 'canton_id' => 2],
-            ['name' => 'Bern', 'img' => 'bern.png', 'ecusson' => 'bern-ecu.png', 'population' => 130000, 'coord' => '46.948020, 7.447433', 'canton_id' => 3],
-            ['name' => 'Lucerne', 'img' => 'lucerne.png', 'ecusson' => 'lucerne-ecu.png', 'population' => 82000, 'coord' => '47.050168, 8.309307', 'canton_id' => 4]
+            ['name' => 'Lausanne', 'img' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Lausanne_img_0585.jpg/1200px-Lausanne_img_0585.jpg', 'ecusson' => 'lausanne-ecu.png', 'population' => 140000, 'coord' => '46.519961, 6.633597', 'canton_id' => 23],
+            ['name' => 'Zurich', 'img' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/City_Z%C3%BCrich_mit_Z%C3%BCrichsee.jpg/1200px-City_Z%C3%BCrich_mit_Z%C3%BCrichsee.jpg', 'ecusson' => 'zurich-ecu.png', 'population' => 400000, 'coord' => '47.376886, 8.541694', 'canton_id' => 2],
+            ['name' => 'Bern', 'img' => 'https://upload.wikimedia.org/wikipedia/commons/e/eb/Central_Bern_from_north.jpg', 'ecusson' => 'bern-ecu.png', 'population' => 130000, 'coord' => '46.948020, 7.447433', 'canton_id' => 3],
+            ['name' => 'Lucerne', 'img' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/1_lucerne_panorama_2012.jpg/1200px-1_lucerne_panorama_2012.jpg?20120709151245', 'ecusson' => 'lucerne-ecu.png', 'population' => 82000, 'coord' => '47.050168, 8.309307', 'canton_id' => 4]
         ];
 
         foreach ($cities as $city)
@@ -30,6 +30,6 @@ class VilleSeeder extends Seeder
                 'canton_id' => $city["canton_id"]
             ));
         }
-        
+
     }
 }
