46a204152e4a2cb6764ae3df3196ded703ade957
mathias <mathias.salmon@he-arc.ch>
2023-11-27 22:15:58 +0100

    creation component search input de base + systeme de services pour centraliser les données

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -2,18 +2,44 @@
   <div class="container">
     <h1>Swissdle</h1>
     <p>Guess the City!</p>
-    <DailyCity></DailyCity>
-
+    <DailyCity />
+    <SearchInput />
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import CityService from "./services/CityService";
+
 import DailyCity from './components/DailyCity.vue'
+import SearchInput from './components/SearchInput.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      apiBaseUrl: "http://localhost/api/",
+      apiGetDailyCity: "ville/today"
+    };
+  },
   components: {
-    DailyCity
+    DailyCity,
+    SearchInput,
+  },
+  methods: {
+    setDailyCity() {
+      try {
+        axios.get(this.apiBaseUrl + this.apiGetDailyCity).then((response) => {
+          (CityService.setDailyCity(response.data))
+        })
+      } catch (err) {
+        alert(err);
+        console.log(err);
+      }
+    }
+  },
+  mounted() {
+    this.setDailyCity();
   }
 }
 </script>
