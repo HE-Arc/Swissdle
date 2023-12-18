90c20fd76c5155746ad4cdbf2c1b587725ed6763
mathias <mathias.salmon@he-arc.ch>
2023-12-19 00:10:31 +0100

    update lang display

--- frontend-vue/src/components/HintsDisplay.vue
+++ frontend-vue/src/components/HintsDisplay.vue
@@ -18,23 +18,24 @@
             </div>
         </div>
         <template v-for="guess in guesses.slice().reverse()" :key="guess.city.id">
-            <div class="hint" :class="(guess.city.name == nameDailyCity ?'good-guess' : 'bad-guess')">
+            <div class="hint" :class="(guess.city.name == nameDailyCity ? 'good-guess' : 'bad-guess')">
                 <div class="city">
                     <p class="montserrat">{{ guess.city.name }}</p>
                 </div>
                 <div class="lang" :class="'hint-' + (guess.lang_diff == 0 ? '0' : '1')">
-                    <p class="montserrat">{{ guess.city.langues }}</p>
+                    <p class="montserrat">{{ formatLanguages(guess.city.langues) }}</p>
                 </div>
                 <div class="canton" :class="'hint-' + (guess.canton_diff ? '0' : '1')">
                     <p class="montserrat">{{ guess.city.canton.name }}</p>
                 </div>
                 <div class="population">
                     <p class="montserrat"> {{ guess.city.population }}</p>
-                    <DirectionalArrow :populationDiff="guess.pop_diff" v-if="guess.pop_diff != 0"/>
+                    <DirectionalArrow :populationDiff="guess.pop_diff" v-if="guess.pop_diff != 0" />
                 </div>
                 <div class="distance hint-grey">
                     <p class="montserrat">~{{ Math.round(guess.position_diff.distance) }}km</p>
-                    <DirectionalArrow :angleToDailyCity="guess.position_diff.direction" v-if="guess.position_diff.distance != 0" />
+                    <DirectionalArrow :angleToDailyCity="guess.position_diff.direction"
+                        v-if="guess.position_diff.distance != 0" />
                 </div>
             </div>
         </template>
@@ -57,6 +58,15 @@ export default {
         DirectionalArrow,
     },
     methods: {
+        formatLanguages(langues) {
+            if (Array.isArray(langues) && langues.length > 0) {
+                const languageNames = langues.map(langue => langue.name);
+
+                return languageNames.join('/');
+            }
+
+            return '';
+        }
     },
     computed: {
         guesses() {
@@ -79,14 +89,14 @@ export default {
     display: flex;
 }
 
-.hint > div {
+.hint>div {
     flex: 1;
     border: 1px solid black;
     padding: 10px 5px;
     margin: 10px 5px;
 }
 
-.hint-titles > div {
+.hint-titles>div {
     flex: 1;
     padding: 10px 5px;
 }
@@ -101,7 +111,7 @@ export default {
     background-color: #FD9696;
 }
 
-.hint.good-guess > div {
+.hint.good-guess>div {
     background-color: #83D881;
 }
 
@@ -110,7 +120,8 @@ export default {
     margin: 0;
 }
 
-.distance, .population {
+.distance,
+.population {
     display: flex;
     justify-content: center;
 }
@@ -131,7 +142,7 @@ export default {
     background-color: #FAE46E;
 }
 
-.hint-grey{
+.hint-grey {
     background-color: #D9D9D9;
 }
 </style>
