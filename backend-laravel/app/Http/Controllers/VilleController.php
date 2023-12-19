6c4447353e26a2e039986a6912ed46eef75a6e46
loic.santschi <loic.santschi@he-arc.ch>
2023-12-19 01:11:24 +0100

    updated the swagger doc

--- backend-laravel/app/Http/Controllers/VilleController.php
+++ backend-laravel/app/Http/Controllers/VilleController.php
@@ -44,6 +44,44 @@ class VilleController extends Controller
         return $ville;
     }
 
+    /**
+     * Make a guess
+     *
+     * @OA\Get(
+     *     path="/api/ville/guess/{id}",
+     *     summary="Get a guess for a city based on the provided ID.",
+     *     description="Retrieve information about the guessed city.",
+     *     operationId="getGuess",
+     *     tags={"Ville"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="The ID of the guessed city.",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *
+     *     @OA\Response(
+     *          response=200,
+     *          description="Successful response",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="ville", ref="#/components/schemas/Ville"),
+     *              @OA\Property(property="canton_diff", type="boolean", description="Whether the guessed city's canton matches the current city's canton"),
+     *              @OA\Property(property="pop_diff", type="integer", description="Population comparison result"),
+     *              @OA\Property(property="lang_diff", type="string", description="Language comparison result"),
+     *              @OA\Property(property="position_diff", type="string", description="Position comparison result"),
+     *          )
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=404,
+     *         description="Ville not found"
+     *     )
+     * )
+     *
+     *  @return App\Utility\CityComparator::compareToToday
+     */
     public function guess($id)
     {
         // TODO: clean id
@@ -51,6 +89,32 @@ class VilleController extends Controller
         return $a->compareToToday($id);
     }
 
+    /**
+     * Get all Villes.
+     *
+     * @OA\Get(
+     *     path="/api/villes",
+     *     summary="Get all cities",
+     *     description="Retrieve information about all cities.",
+     *     operationId="getAll",
+     *     tags={"Ville"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Ville")
+     *         )
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=404,
+     *         description="Ville not found"
+     *     )
+     * )
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function all()
     {
         $villes = Ville::all();
